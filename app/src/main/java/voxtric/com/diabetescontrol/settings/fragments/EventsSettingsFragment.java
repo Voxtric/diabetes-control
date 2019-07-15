698ee2504f92a3205ccc92270f708be135d46e1f
Benjamin James Drury <ben@ben-drury.co.uk>
2019-07-15 13:21:02 +0100

    Implemented code for edit events settings fragment

--- app/src/main/java/voxtric/com/diabetescontrol/settings/fragments/EventsSettingsFragment.java
+++ app/src/main/java/voxtric/com/diabetescontrol/settings/fragments/EventsSettingsFragment.java
@@ -1,5 +1,7 @@
 package voxtric.com.diabetescontrol.settings.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import voxtric.com.diabetescontrol.R;
+import voxtric.com.diabetescontrol.settings.EditEventsActivity;
 
 public class EventsSettingsFragment extends Fragment
 {
@@ -19,15 +22,23 @@ public class EventsSettingsFragment extends Fragment
   }
 
   @Override
-  public void onCreate(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState)
   {
-    return inflater.inflate(R.layout.fragment_events_settings, container, false);
+    View view = inflater.inflate(R.layout.fragment_events_settings, container, false);
+    view.findViewById(R.id.edit_events_button).setOnClickListener(new View.OnClickListener()
+    {
+      @Override
+      public void onClick(View view)
+      {
+        Activity activity = getActivity();
+        if (activity != null)
+        {
+          Intent intent = new Intent(activity, EditEventsActivity.class);
+          startActivity(intent);
+        }
+      }
+    });
+    return view;
   }
 }
