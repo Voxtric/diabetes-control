2a32154b77127f4141b2603153059d15e2997a20
Benjamin James Drury <ben@ben-drury.co.uk>
2019-07-15 18:48:11 +0100

    Implemented validation that BGL range value is a number

--- app/src/main/java/voxtric/com/diabetescontrol/settings/fragments/BGLHighlightingSettingsFragment.java
+++ app/src/main/java/voxtric/com/diabetescontrol/settings/fragments/BGLHighlightingSettingsFragment.java
@@ -3,23 +3,40 @@ package voxtric.com.diabetescontrol.settings.fragments;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import java.text.ParseException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import voxtric.com.diabetescontrol.R;
 import voxtric.com.diabetescontrol.database.DatabaseActivity;
 import voxtric.com.diabetescontrol.database.Preference;
 import voxtric.com.diabetescontrol.settings.SettingsActivity;
+import voxtric.com.diabetescontrol.utilities.CompositeOnFocusChangeListener;
+import voxtric.com.diabetescontrol.utilities.ViewUtilities;
 
 public class BGLHighlightingSettingsFragment extends Fragment
 {
+  private static final Map<String, Float> DEFAULT_VALUES;
+  static
+  {
+    Map<String, Float> defaults = new HashMap<>();
+    defaults.put("ideal_minimum", 2.6f);
+    defaults.put("high_minimum", 8.2f);
+    defaults.put("action_required_minimum", 11.9f);
+    DEFAULT_VALUES = Collections.unmodifiableMap(defaults);
+}
+
   public BGLHighlightingSettingsFragment()
   {
     // Required empty public constructor
@@ -39,6 +56,11 @@ public class BGLHighlightingSettingsFragment extends Fragment
       final EditText highRangeLower = view.findViewById(R.id.high_range_lower);
       final EditText highRangeUpper = view.findViewById(R.id.high_range_upper);
 
+      ViewUtilities.addHintHide(idealRangeLower, Gravity.CENTER, activity);
+      ViewUtilities.addHintHide(idealRangeUpper, Gravity.CENTER, activity);
+      ViewUtilities.addHintHide(highRangeLower, Gravity.CENTER, activity);
+      ViewUtilities.addHintHide(highRangeUpper, Gravity.CENTER, activity);
+
       LinkedEditTextsTextWatcher idealRangeUpperTextWatcher = new LinkedEditTextsTextWatcher(highRangeLower);
       LinkedEditTextsTextWatcher highRangeLowerTextWatcher = new LinkedEditTextsTextWatcher(idealRangeUpper);
       idealRangeUpperTextWatcher.setLinkedEditTextTextWatcher(highRangeLowerTextWatcher);
@@ -52,8 +74,8 @@ public class BGLHighlightingSettingsFragment extends Fragment
       saveValuesToDatabaseWhenUnfocused(activity, highRangeUpper, "action_required_minimum");
 
       Preference.get(activity,
-          new String[]{"ideal_minimum", "high_minimum", "action_required_minimum"},
-          new String[]{"2.6", "8.2", "11.9"},
+          new String[]{ "ideal_minimum", "high_minimum", "action_required_minimum" },
+          new String[]{ String.valueOf(DEFAULT_VALUES.get("ideal_minimum")), String.valueOf(DEFAULT_VALUES.get("high_minimum")), String.valueOf(DEFAULT_VALUES.get("action_required_minimum")) },
           new Preference.ResultRunnable()
           {
             @Override
@@ -71,16 +93,44 @@ public class BGLHighlightingSettingsFragment extends Fragment
     return view;
   }
 
-  private void saveValuesToDatabaseWhenUnfocused(final DatabaseActivity activity, final EditText view, final String prefrenceName)
+  private void saveValuesToDatabaseWhenUnfocused(final DatabaseActivity activity, final EditText view, final String preferenceName)
   {
-    view.setOnFocusChangeListener(new View.OnFocusChangeListener()
+    CompositeOnFocusChangeListener.applyListenerToView(view, new View.OnFocusChangeListener()
     {
       @Override
       public void onFocusChange(View v, boolean hasFocus)
       {
         if (!hasFocus)
         {
-          Preference.put(activity, prefrenceName, view.getText().toString(), null);
+          String text = view.getText().toString();
+          if (text.endsWith("."))
+          {
+            text += "0";
+          }
+
+          try
+          {
+            float value = Float.valueOf(text);
+
+            // TODO: Validate for higher than 0
+            // TODO: Validate for higher or lower than previous or next value
+
+
+            Preference.put(activity, preferenceName, text, null);
+            view.setText(String.valueOf(value));
+          }
+          catch (NumberFormatException ignored)
+          {
+            Preference.get(activity, preferenceName, String.valueOf(DEFAULT_VALUES.get(preferenceName)), new Preference.ResultRunnable()
+            {
+              @Override
+              public void run()
+              {
+                view.append(getResult());
+              }
+            });
+            Toast.makeText(activity, R.string.bgl_range_value_empty_message, Toast.LENGTH_LONG).show();
+          }
         }
       }
     });
